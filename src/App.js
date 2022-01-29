caa03dc1ed5dde7d9154bd791942c02447d6d3ae
manishmotwani2002 <manishmotwani2002@gmail.com>
2022-01-29 14:09:43 +0530

    razorpay added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React,{createContext,useReducer} from 'react'
+import React, { createContext, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Routes, Route,  BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import {
+	Routes,
+	Route,
+	BrowserRouter,
+	Switch,
+	Redirect,
+} from 'react-router-dom';
 import Navbar from './Components/base/Navbars';
 import Home from './views/Home/Home';
 import About from './Components/About';
@@ -14,46 +20,48 @@ import Login from './views/Auth/Login';
 import Logout from './views/Auth/Logout';
 import Signup from './views/Auth/Signup';
 import Profile from './Components/Profile';
-import Footer from "./Components/base/Footer";
+import Footer from './Components/base/Footer';
 // import ErrorPage from './Components/base/ErrorPage';
 import ScrollButton from './Components/ScrollButton';
-import AddCourse from "./Admin/Course/AddCourse";
-import UpdateCourse from "./Admin/Course/UpdateCourse";
-import { initialState,reducer } from './reducer/UseReducer';
-import PrivateRoute from "./APIs/auth/PrivateRoutes"
+import AddCourse from './Admin/Course/AddCourse';
+import UpdateCourse from './Admin/Course/UpdateCourse';
+import { initialState, reducer } from './reducer/UseReducer';
+import PrivateRoute from './APIs/auth/PrivateRoutes';
+import PaymentGateway from './views/payment/PaymentGateway';
 
 export const UserContext = createContext();
-const Routing = ()=>{
-  return (
-    <Routes>
-      <Route path="/addCourse" element={<AddCourse />} />
-      <Route path="/updateCourse" element={<UpdateCourse />} />
-      <Route exact path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/studymaterial" element={<Studymaterial />} />
-      <Route path="/studymaterial/iit" element={<Studymaterialiit />} />
-      <Route path="/studymaterial/neet" element={<Studymaterialneet />} />
-      <Route path="/testseries" element={<Testseries />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/logout" element={<Logout />} />
-    </Routes>
-  );
+const Routing = () => {
+	return (
+		<Routes>
+			<Route path="/addCourse" element={<AddCourse />} />
+			<Route path="/updateCourse" element={<UpdateCourse />} />
+			<Route exact path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/studymaterial" element={<Studymaterial />} />
+			<Route path="/studymaterial/iit" element={<Studymaterialiit />} />
+			<Route path="/studymaterial/neet" element={<Studymaterialneet />} />
+			<Route path="/testseries" element={<Testseries />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="/profile" element={<Profile />} />
+			<Route path="/logout" element={<Logout />} />
+			<Route path="/payment" element={<PaymentGateway />} />
+		</Routes>
+	);
 };
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
- 
-  return (
-    <>
-    <UserContext.Provider value={{state,dispatch}}>
-      <Navbar />
-     <Routing />
-      <Footer />
-      <ScrollButton />
-    </UserContext.Provider>
-    </>
-  )
-}
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<>
+			<UserContext.Provider value={{ state, dispatch }}>
+				<Navbar />
+				<Routing />
+				<Footer />
+				<ScrollButton />
+			</UserContext.Provider>
+		</>
+	);
+};
 export default App;
